83218981de003e9fcadb77a1151a4bfe2e38d21d
Mel Racho <melracho@Mel-Rachos-MacBook-Pro-5.local>
2013-09-23 19:58:12 -0400

    row index

---
+++ index.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Grid Dev - PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="assets/css/bootstrap-lightbox.css" rel="stylesheet" media="screen">
+    <link href="assets/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="wrapper">
+        <h1>12-grid</h1>
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" 		aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title">Modal title</h4>
+        </div>
+        <div class="modal-body">
+		<img src="http://placehold.it/500x300">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+	
+  <div class="row">
+  		
+  		<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a data-toggle="modal" href="#myModal" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	</div>
+    	
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4  col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+	    	
+    	</div>
+    	
+  </div><!--row-->
+    <div class="row">
+  		
+  		<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4  col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+	    	
+    	</div>
+    	
+  </div><!--row-->
+    <div class="row">
+  		
+  		<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4  col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+	    	
+    	</div>
+    	
+  </div><!--row-->
+  <div class="row">
+  		<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4  col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+	    	
+    	</div>
+    	
+    	
+  </div><!--row-->  
+  
+  <?php 
+  include 'row.php'; 
+  ?>
+  
+  </div><!--wrapper-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap-lightbox.js"></script>
+    <script src="assets/js/main.js"></script>
+  </body>
+</html>
---
+++ row.php
@@ -0,0 +1,75 @@
+
+<div class="row">
+  		<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4  col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+    	
+    	</div>
+    	<div class="col-xs-4 col-sm-2 col-md-1 col-lg-1">
+  		<a href="#" class="thumbnail">
+  				<img src="http://placehold.it/200x200">
+    	</a>
+	    	
+    	</div> 	
+  </div><!--row--> 
